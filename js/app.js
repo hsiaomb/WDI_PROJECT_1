99d0699a2dda919c6a79653dcfbdcf35a1c966be
hsiaomb <hsiaomb@usc.edu>
2015-10-13 12:06:32 +0100

    got functionality of it working

--- js/app.js
+++ js/app.js
@@ -1,155 +1,126 @@
 $(function(){
-  var game ={
-    playerOneSequence: [],
-    playerTwoSequence:[],
-    playerOneRepeat:[],
-    playerTwoRepeat:[],
-    round: 0,
-    turnCounter: 0,
-    playerOneScore: 0,
-    playerTwoScore: 0,
-
-    playerOneToArray: function(){
-     document.addEventListener('keypress', function(e) {
-       game.playerOneSequence.push(e.keyCode);
-       console.log(game.playerOneSequence.slice(0,4));
-       if (game.playerOneSequence.length === 4){
-
-        for (var i = 0; i <= game.playerOneSequence.length; i++) {
-            (function(index) {
-                setTimeout(function() {game.keyboardSequence(game.playerOneSequence[index]); }, i * 1000);
-            })(i);
-        }
-
-     //    for(var i = 0; i < game.playerOneSequence.length; i++){
-
-     // setTimeout(function(){
-     //  console.log(game.playerOneSequence[i])}, 1000*i);
-     
-     //        // keyboard(game.playerOneSequence[i]);
-               game.playerTwoRepeatArray();
-             };
-          
-        // game.playerOneSequence.slice(0,4).trigger("click")
-      })
-   },
-
-   playerTwoRepeatArray: function(){
-    document.addEventListener('keypress', function(e) {
-      game.playerTwoRepeat.push(e.keyCode);
-      console.log(game.playerTwoRepeat.slice(0,4));
-      if (game.playerTwoRepeat.slice(0,4).toString() === game.playerOneSequence.slice(0,4).toString()){
-        console.log("Match");
-      }
-      else {
-        console.log("Not a match!");
-        playerOneScore++;
-        $("#data-string").innerText(playerOneScore);
-
-
-
-      }
-    })
-
-
-
-  },
-  repeatKeyboardSequence: function(){$('html').keypress(function(e){
-    e = e || window.event;
-   switch (keycode) {
-     case 119:
-     $("#high-tom").trigger("click");
-     break;
-     case 97:
-     $("#high-hat").trigger("click");
-     break;
-     case 115:
-     $("#snare-drum").trigger("click");
-     break;
-     case 100:
-     $("#bass-drum").trigger("click");
-     break;
-   }
- })
- },
-  keyboardSequence: function(keycode){
-   switch (keycode) {
-     case 119:
-     $("#high-tom").trigger("click");
-     break;
-     case 97:
-     $("#high-hat").trigger("click");
-     break;
-     case 115:
-     $("#snare-drum").trigger("click");
-     break;
-     case 100:
-     $("#bass-drum").trigger("click");
-     break;
-  // keyboard: function(){$('html').keypress(function(e){
-  //   e = e || window.event;
-  //   switch (event.keyCode) {
-  //     case 119:
-  //         $("#high-tom").trigger("click");
-  //         break;
-  //     case 97:
-  //         $("#high-hat").trigger("click");
-  //         break;
-  //     case 115:
-  //         $("#snare-drum").trigger("click");
-  //         break;
-  //     case 100:
-  //         $("#bass-drum").trigger("click");
-  //         break;
-}
-},
-
-arrow: function(){$('html').keydown(function(e){
-  e = e || window.event;
-  switch (event.keyCode) {
-    case 37:
-    event.preventDefault;
-    $("#high-hat").trigger("click");
-    break;
-    case 38:
-    event.preventDefault;
-    $("#high-tom").trigger("click");
-    break;
-
-    case 39:
-    event.preventDefault;
-    $("#bass-drum").trigger("click");
-    break;
-    case 40:
-    event.preventDefault;
-    $("#snare-drum").trigger("click");
-    break;
-  }
-})
-},
-playSound: function(){
-  var buttons = document.getElementsByTagName("td");
+  var buttons = $("td");
   soundManager.setup({
     url: "/swf/",
     preferFlash: true
   });
 
-  for(var i = buttons.length -1; i >= 0; i--){
-    buttons[i].addEventListener("click", playSong)
-  }
+  $(buttons).on("click", playSong)
 
   function playSong(){
+    var song = $(this).attr("data-song");
     soundManager.createSound({
-      id: event.target.getAttribute("data-song"),
-      url: "./sounds/" + event.target.getAttribute("data-song") + ".wav"
+      id: song,
+      url: "./sounds/" + song + ".wav"
     }).play();
   }
-}
-}
-game.playSound();
-game.arrow();
-game.playerOneToArray();
+  $('h2').hide();
+  $('h3').hide();
 
+
+  var game ={
+
+    beatCounter: 1,
+    playerOneSequence: [],
+    playerTwoSequence:[],
+    storedMoves:[],
+    playerTwoRepeat:[],
+    round: 0,
+    playerTwoScore: 0,
+    roundNum: $('#0'),
+    playerTwoScoreboard: $('#2'),
+    keyboardMap: {
+     119:  "#high-tom",
+     97: "#high-hat",
+     115: "#snare-drum",
+     100: "#bass-drum"
+   },
+   arrowMap:{
+     38:  "#high-tom",
+     37: "#high-hat",
+     40: "#snare-drum",
+     39: "#bass-drum"
+   },
+
+   startGame: function(){
+    event.preventDefault();
+    $('h2').fadeIn();
+    $('h3').fadeIn();
+    $('#start').fadeOut();
+    game.round++;
+    game.roundNum.text(game.round);
+
+    game.playerOneToArray();
+
+  },
+  playerOneToArray: function(){
+   $(document).on('keypress', function(e) {
+    if([119,100,115,97].indexOf(e.keyCode)!== -1){
+     game.playerOneSequence.push(game.keyboardMap[e.keyCode]);
+     console.log(game.playerOneSequence);
+     if (game.playerOneSequence.length === game.beatCounter){
+      for (var i = 0; i < game.playerOneSequence.length; i++) {
+        (function(index) {
+          setTimeout(function() {game.keyboardSequence(game.playerOneSequence[index]); }, i * 650);
+        })(i);
+      }
+      $(document).off('keypress');
+      game.playerTwoRepeatArray();
+    };
+
+  }
+})
+ },
+
+ playerTwoRepeatArray: function(){
+   $(document).on('keydown', function(e) {
+    if([37,38,39,40].indexOf(e.keyCode) !== -1){
+     game.playerTwoRepeat.push(game.arrowMap[e.keyCode]);
+     $(game.arrowMap[e.keyCode]).trigger("click");
+     console.log(game.playerTwoRepeat);
+     for (var i = 0; i < game.playerTwoRepeat.length; i++) {
+      console.log(game.playerTwoRepeat[i]);
+      console.log(game.playerOneSequence[i]);
+      if(game.playerTwoRepeat[i] !== game.playerOneSequence[i]) {
+        alert("you lose!");
+      }
+      else if(game.playerTwoRepeat.slice(0,game.beatCounter).toString()=== game.playerOneSequence.slice(0,game.beatCounter).toString()){
+        alert("Nice! Next Round!");
+        game.playerTwoScore++;
+        game.playerTwoScoreboard.text(game.playerTwoScore);
+        game.playerOneSequence;
+        console.log(game.playerTwoRepeat);
+        console.log(game.playerOneSequence);
+        console.log(game.beatCounter)
+        $(document).off('keydown');
+        game.nextRound();
+
+      }
+    };
+
+  }
+})
+
+ },  
+
+ nextRound: function(){
+  game.playerTwoRepeat = [];
+  game.playerOneSequence = [];
+  game.beatCounter++;
+  game.playerOneToArray();
+  game.round++;
+  game.roundNum.text(game.round);
+  console.log(game.playerTwoRepeat);
+  console.log(game.playerOneSequence);
+  console.log(game.beatCounter)
+
+},
+
+keyboardSequence: function(id){
+ $(id).trigger("click");
+},
+}
+$('#start').on('click', game.startGame);
 })
 
 
